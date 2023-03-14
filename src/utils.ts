237de42f177ff31d4d437f47c64d427fd83a24cf
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-03-14 18:57:20 +0100

    cleanup

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const API_URL_BOTDETECTOR = "/api/osrsbotdetector";
-export const API_URL_RUNELITE = "/api/runelite";
-
-export const replaceUnderscoreWithSpace = (input: string): string =>
-  input.replace(/_/g, " ");
-export const toPercentage = (input: number): string =>
-  `${(input * 100).toFixed(2)}%`;
