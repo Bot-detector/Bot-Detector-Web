70e1b1eaf5696c006bf2af1c1f80cf90bc419c01
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-03-25 18:04:17 +0100

    faq

--- src/api/account_search.py
+++ src/api/account_search.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
 from src.core.config import CONFIG, templates
 
@@ -6,5 +7,5 @@ router = APIRouter()
 
 
 @router.get("/")
-async def account_search(request: Request):
+async def account_search(request: Request) -> HTMLResponse:
     return templates.TemplateResponse("pages/account_search.html", {"request": request})
