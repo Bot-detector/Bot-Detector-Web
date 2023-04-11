68b55b31db1d2b1d3ed68eb15400b8dd31844a1f
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-04-11 22:59:18 +0200

    behind nginx

--- src/core/server.py
+++ src/core/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
@@ -42,5 +42,5 @@ app = create_app()
 
 
 @app.get("/")
-def root():
-    return RedirectResponse(url="/home")
+def root(request:Request):
+    return RedirectResponse(request.url_for("home"))
