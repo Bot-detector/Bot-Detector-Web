9c9da3bee5fbc5bf1fd197f1f3703b584fd41541
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-04-13 22:27:24 +0200

    https_url_for if base_url is https

--- src/core/config.py
+++ src/core/config.py
@@ -5,11 +5,28 @@ from fastapi.templating import Jinja2Templates
 from src.app.controllers.bot_detector import BotDetector
 from src.app.controllers.patreon import Patreon
 import dotenv
+import jinja2
+from fastapi import Request
 
 dotenv.load_dotenv()
 
-templates = Jinja2Templates(directory="src/templates")
 
+
+if hasattr(jinja2, "pass_context"):
+    pass_context = jinja2.pass_context
+else:
+    pass_context = jinja2.contextfunction
+
+@pass_context
+def https_url_for(context: dict, name: str, **path_params) -> str:
+    request:Request = context["request"]
+    http_url = request.url_for(name, **path_params)
+    if 'https' in str(request.base_url):
+        http_url = str(http_url).replace("http", "https")
+    return http_url
+
+templates = Jinja2Templates(directory="src/templates")
+templates.env.globals["url_for"] = https_url_for
 
 class Config(BaseSettings):
     BD_TOKEN: str
