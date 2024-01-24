7f6234b33e40695686ef995120435663df160e19
mitsuki-jpg <m1tsuki@int.pl>
2024-01-24 22:12:55 +0100

    Card

---
+++ src/api/cookies.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+from src.core.config import CONFIG, templates
+
+router = APIRouter()
+
+
+@router.get("/")
+async def contact(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("pages/cookies.html", {"request": request})
